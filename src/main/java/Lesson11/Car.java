833a740588934173e0c8ea30400c2d98e858c0e9
jborzykh <jborzykh@mail.ru>
2023-01-22 19:11:44 +0300

    lesson 12 HW

--- src/main/java/Lesson11/Car.java
+++ src/main/java/Lesson11/Car.java
@@ -4,10 +4,10 @@ package Lesson11;
 
 public class Car {
     String color;
-    String engine;
+    int engine;
     public  int doors;
 
-    public Car(String color, String engine, int doors) {
+    public Car(String color, int engine, int doors) {
         this.color = color;
         this.engine = engine;
         this.doors = doors;
@@ -32,13 +32,32 @@ class CarTest {
 
     public static void main(String[] args) {
         CarTest cT = new CarTest();
-        Car c1 = new Car("green", "v6", 4);
-        Car c2 = new Car("red", "v8", 3);
+        Car c1 = new Car("green", 4, 4);
+        Car c2 = new Car("red", 3, 6);
         cT.changeDoors(c1, 6);
         cT.changeColor(c1, c2);
         System.out.println(c1.doors);
         System.out.println(c1.color);
         System.out.println(c2.color);
+
+        //12 урок
+        if(c1.engine > c2.engine) {
+            if (c1.doors > c2.doors) {
+                System.out.println("Car1 is cooler than Car2");
+            }
+            else{
+                System.out.println("Car2 is cooler than Car1");
+            }
+        }
+        else{
+            System.out.println("cars are cool");
+        }
+        if (c1.color.equals(c2.color)){
+            System.out.println("Color");
+        }
+        else{
+            System.out.println("Different colors");
+        }
 
 
     }
