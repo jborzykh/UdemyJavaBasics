ab148438142f6b98433da9abda3a1e1767a59177
jborzykh <jborzykh@mail.ru>
2023-02-16 15:01:49 +0300

    lesson 18

--- src/main/java/Lesson16/HomeWork.java
+++ src/main/java/Lesson16/HomeWork.java
@@ -24,3 +24,35 @@ public class HomeWork {
 
 
 
+
+//        char c1;
+//        String strEmail;
+//        try {
+//            for (int i = 0; i < email.length(); i++) {
+//                c1 = email.charAt(i);
+//
+//                if (c1 == ' ') {
+//                    System.out.println();
+//                    System.out.print(c1);
+//                    continue;
+//
+//                }
+//                strEmail = String.valueOf(c1);
+//                String resultEmail = strEmail.trim().substring(strEmail.indexOf("@") + 1, strEmail.indexOf("."));
+//                System.out.print(resultEmail);
+//
+//            }
+//        } catch (StringIndexOutOfBoundsException e) {
+//            System.out.println("String index out of bounds. String length: " + email.length());
+//        }
+
+//    public static String getMail(String email){
+//        String charsetStr = "@";
+//        int start = email.indexOf(charsetStr) + charsetStr.length();
+//        int end  = email.indexOf('.', start);
+//        if (start < 0 || end < 0){
+//            return "";
+//        }
+//        String encoding = email.substring(start,end);
+//        return encoding;
+//    }
