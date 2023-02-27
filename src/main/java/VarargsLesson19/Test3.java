38167766cb2a7a178c84f5ae925e893db206c7f9
jborzykh <jborzykh@mail.ru>
2023-02-27 15:10:25 +0300

    lesson 19 HW

--- src/main/java/VarargsLesson19/Test3.java
+++ src/main/java/VarargsLesson19/Test3.java
@@ -1,4 +1,13 @@
 package VarargsLesson19;
 
 public class Test3 {
+    public static void main(String[] args) {
+        int [] array = {20, 1 ,0, 18,2};
+        int summa = 0;
+        for (int a : array){
+            summa += a;
+
+        }
+        System.out.println(summa);
+    }
 }
